311639ca9ff3a3d3166f76aaa35f9a9201559bad
tth05 <36999320+tth05@users.noreply.github.com>
2020-10-13 20:10:35 +0200

    added proper zooming and scrolling

--- src/main/java/com/github/tth05/codeviewer/gui/CodeViewScreen.java
+++ src/main/java/com/github/tth05/codeviewer/gui/CodeViewScreen.java
@@ -2,18 +2,34 @@ package com.github.tth05.codeviewer.gui;
 
 import com.github.tth05.codeviewer.util.CodeHighlighter;
 import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.client.renderer.GlStateManager;
-import org.lwjgl.opengl.GL11;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 
 import java.io.IOException;
 import java.util.List;
 
 public class CodeViewScreen extends GuiScreen {
 
-    private String filename;
-    private List<String> lines;
+    private CodeTextField codeTextField;
+    private Scrollbar scrollbar;
 
-    float scale = 1f;
+    private float scale = 1f;
+
+    @Override
+    public void initGui() {
+        super.initGui();
+
+        List<String> oldLines = null;
+        if (codeTextField != null)
+            oldLines = codeTextField.getLines();
+
+        codeTextField = new CodeTextField(5, 5, this.width - 10, this.height - 10);
+
+        if (oldLines != null)
+            codeTextField.setLines(oldLines);
+
+        scrollbar = new Scrollbar(0, oldLines == null ? 1 : oldLines.size() - 1);
+    }
 
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
@@ -21,25 +37,10 @@ public class CodeViewScreen extends GuiScreen {
 
         drawRect(0, 0, this.width, this.height, 0xFF282C34);
 
-        if(lines == null)
-            return;
-
-
         boolean prev = this.fontRenderer.getUnicodeFlag();
         this.fontRenderer.setUnicodeFlag(false);
 
-        GlStateManager.pushMatrix();
-        GlStateManager.scale(scale, scale, 1);
-        GlStateManager.enableBlend();
-        GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-
-        int y = 5;
-        for (String line : lines) {
-            this.fontRenderer.drawString(line, Math.round(5f / scale), y, 0xFFFFFFFF);
-            y += 9;
-        }
-
-        GlStateManager.popMatrix();
+        this.codeTextField.draw(this.fontRenderer, scale, scrollbar.getOffset());
 
         this.fontRenderer.setUnicodeFlag(prev);
     }
@@ -47,24 +48,35 @@ public class CodeViewScreen extends GuiScreen {
     @Override
     public void handleMouseInput() throws IOException {
         super.handleMouseInput();
+        int delta = Mouse.getEventDWheel();
+
+        if (!Keyboard.isKeyDown(Keyboard.KEY_LCONTROL)) {
+            scrollbar.mouseWheel(delta);
+            return;
+        }
+
+        if (delta > 0)
+            scale += 0.025f;
+        else if (delta < 0)
+            scale -= 0.025f;
+
+        updateScrollbarParameters();
+    }
+
+    private void updateScrollbarParameters() {
+        scrollbar.setMax(Math.max(0, this.codeTextField.getLines().size() - this.codeTextField.getVisibleRows(scale)));
+        scrollbar.setStepSize(Scrollbar.DEFAULT_STEP_SIZE / scale);
     }
 
     @Override
     protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
         super.mouseClicked(mouseX, mouseY, mouseButton);
-
-        if(mouseButton == 0)
-            scale += .05f;
-        else
-            scale -= .05f;
     }
 
     public void setLines(List<String> lines) {
-        this.lines = lines;
         CodeHighlighter.highlightJavaCode(lines);
-    }
+        this.codeTextField.setLines(lines);
 
-    public void setFilename(String filename) {
-        this.filename = filename;
+        updateScrollbarParameters();
     }
 }
--- src/main/java/com/github/tth05/codeviewer/gui/Scrollbar.java
+++ src/main/java/com/github/tth05/codeviewer/gui/Scrollbar.java
@@ -1,3 +1,55 @@
 package com.github.tth05.codeviewer.gui;
+
 public class Scrollbar {
+
+    public static final float DEFAULT_STEP_SIZE = 0.06f;
+
+    private int min;
+    private int max;
+
+    /**
+     * a value between 0 and 1 that defines how much of the available range should be added each scroll
+     * event to the offset. Basically the speed of the scrollbar.
+     */
+    private float stepSize = DEFAULT_STEP_SIZE;
+
+    private float currentOffset;
+
+    /**
+     * @param min      the minimum
+     * @param max      the maximum
+     */
+    public Scrollbar(int min, int max) {
+        this.min = min;
+        this.max = max;
+
+        this.currentOffset = min;
+    }
+
+    public void mouseWheel(int delta) {
+        if (delta < 0) {
+            this.currentOffset = Math.min(max, this.currentOffset + (max - min) * stepSize);
+        } else if (delta > 0) {
+            this.currentOffset = Math.max(min, this.currentOffset - (max - min) * stepSize);
+        }
+    }
+
+    public int getOffset() {
+        return (int) currentOffset;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+
+        if (this.currentOffset > max)
+            this.currentOffset = max;
+    }
+
+    public void setStepSize(float stepSize) {
+        this.stepSize = stepSize;
+    }
 }
