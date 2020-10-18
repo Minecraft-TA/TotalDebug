e53ae499193db3cb542b59db2d86e8d5ca9062a6
Pelotrio <45769595+Pelotrio@users.noreply.github.com>
2020-10-18 21:38:37 +0200

    Update KeyBindings.java

--- src/main/java/com/github/tth05/codeviewer/KeyBindings.java
+++ src/main/java/com/github/tth05/codeviewer/KeyBindings.java
@@ -10,7 +10,7 @@ public class KeyBindings {
     public static final KeyBinding CODE_GUI =
             new KeyBinding("key." + CodeViewer.MOD_ID + ".openCodeGui", KeyConflictContext.IN_GAME, Keyboard.KEY_F6, CodeViewer.MOD_NAME);
     public static final KeyBinding LOADED_GUI =
-            new KeyBinding("key." + CodeViewer.MOD_ID + ".openEntityGui", KeyConflictContext.IN_GAME, Keyboard.KEY_F7, CodeViewer.MOD_ID);
+            new KeyBinding("key." + CodeViewer.MOD_ID + ".openEntityGui", KeyConflictContext.IN_GAME, Keyboard.KEY_F7, CodeViewer.MOD_NAME);
 
     public static void init() {
         ClientRegistry.registerKeyBinding(CODE_GUI);
