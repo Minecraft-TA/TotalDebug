dea8259ddab944aec84277f698f4532bcc587dde
Jan <ForGermany@gmx.de>
2020-10-18 15:58:33 +0200

    Second half of minor fixes

---
+++ src/main/java/com/github/tth05/codeviewer/HitType.java
@@ -0,0 +1,7 @@
+package com.github.tth05.codeviewer;
+
+public enum HitType {
+    BLOCK_ENTITY,
+    TILE_ENTITY,
+    LIVING_ENTITY
+}
--- src/main/java/com/github/tth05/codeviewer/network/DecompilationRequestMessage.java
+++ src/main/java/com/github/tth05/codeviewer/network/DecompilationRequestMessage.java
@@ -1,22 +1,21 @@
 package com.github.tth05.codeviewer.network;
 
 import com.github.tth05.codeviewer.CodeViewer;
+import com.github.tth05.codeviewer.HitType;
 import com.github.tth05.codeviewer.util.ByteBufHelper;
 import io.netty.buffer.ByteBuf;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeChunkManager;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 
 public class DecompilationRequestMessage implements IMessage, IMessageHandler<DecompilationRequestMessage, IMessage> {
 
-    public enum HitType {
-        BLOCK_ENTITY,
-        TILE_ENTITY,
-        LIVING_ENTITY
-    }
 
     private HitType typeOfHit;
     private BlockPos pos;
@@ -26,22 +25,27 @@ public class DecompilationRequestMessage implements IMessage, IMessageHandler<De
     public DecompilationRequestMessage() {
     }
 
-    public DecompilationRequestMessage(HitType typeofhit, BlockPos pos, int entityID) {
+    public DecompilationRequestMessage(HitType typeofhit, BlockPos pos) {
         this.typeOfHit = typeofhit;
         this.pos = pos;
-        this.entityId = entityID;
+        this.entityId = -1;
+    }
+
+    public DecompilationRequestMessage(HitType typeOfHit, int entityId) {
+        this.typeOfHit = typeOfHit;
+        this.entityId = entityId;
     }
 
     @Override
     public void fromBytes(ByteBuf buf) {
-        this.typeOfHit = HitType.valueOf(ByteBufHelper.readUTF8String(buf));
+        this.typeOfHit = HitType.values()[buf.readInt()];
         this.pos = new BlockPos(buf.readInt(), buf.readInt(), buf.readInt());
         this.entityId = buf.readInt();
     }
 
     @Override
     public void toBytes(ByteBuf buf) {
-        ByteBufHelper.writeUTF8String(buf, this.typeOfHit.name());
+        buf.writeInt(this.typeOfHit.ordinal());
         buf.writeInt(this.pos.getX());
         buf.writeInt(this.pos.getY());
         buf.writeInt(this.pos.getZ());
@@ -53,15 +57,27 @@ public class DecompilationRequestMessage implements IMessage, IMessageHandler<De
         EntityPlayerMP player = ctx.getServerHandler().player;
         World world = player.world;
 
+        ForgeChunkManager.getPersistentChunksFor(world);
+
         switch (typeOfHit) {
             case BLOCK_ENTITY:
                 sendToClient(world.getBlockState(message.pos).getBlock().getClass(), player);
                 break;
             case TILE_ENTITY:
-                sendToClient(world.getTileEntity(message.pos).getClass(), player);
+                TileEntity tileEntity = world.getTileEntity(message.pos);
+                if (tileEntity != null) {
+                    sendToClient(tileEntity.getClass(), player);
+                } else {
+                    CodeViewer.LOGGER.error("TileEntity is null");
+                }
                 break;
             case LIVING_ENTITY:
-                sendToClient(world.getEntityByID(message.entityId).getClass(), player);
+                Entity entity = world.getEntityByID(message.entityId);
+                if (entity != null) {
+                    sendToClient(entity.getClass(), player);
+                } else {
+                    CodeViewer.LOGGER.error("Entity is null");
+                }
                 break;
         }
         return null;
