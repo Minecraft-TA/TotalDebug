e956639cc7b8e7c65501e3b82a2748f3da9e35df
tth05 <36999320+tth05@users.noreply.github.com>
2020-11-02 17:08:11 +0100

    actually register tile entity

--- src/main/java/com/github/minecraft_ta/totaldebug/proxy/CommonProxy.java
+++ src/main/java/com/github/minecraft_ta/totaldebug/proxy/CommonProxy.java
@@ -1,14 +1,19 @@
 package com.github.minecraft_ta.totaldebug.proxy;
 
 import com.github.minecraft_ta.totaldebug.TotalDebug;
+import com.github.minecraft_ta.totaldebug.block.tile.TickBlockTile;
 import com.github.minecraft_ta.totaldebug.network.*;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 
 public class CommonProxy {
 
     public void preInit(FMLPreInitializationEvent e) {
+        GameRegistry.registerTileEntity(TickBlockTile.class, new ResourceLocation(TotalDebug.MOD_ID, "tick_block_tile"));
+
         int id = 0;
         TotalDebug.INSTANCE.network.registerMessage(TicksPerSecondMessage.class, TicksPerSecondMessage.class, id++, Side.CLIENT);
 
