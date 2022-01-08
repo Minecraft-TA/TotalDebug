503fc1c34e575568286328bbf3466f2440413cc2
Pelotrio <45769595+Pelotrio@users.noreply.github.com>
2022-01-08 02:12:18 +0100

    TPS on tab now also works in singleplayer

--- src/main/java/com/github/minecraft_ta/totaldebug/handler/TabOverlayRenderHandler.java
+++ src/main/java/com/github/minecraft_ta/totaldebug/handler/TabOverlayRenderHandler.java
@@ -2,6 +2,8 @@ package com.github.minecraft_ta.totaldebug.handler;
 
 import com.github.minecraft_ta.totaldebug.TotalDebug;
 import com.github.minecraft_ta.totaldebug.network.TickTimeRequestMessage;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.ScaledResolution;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -11,13 +13,31 @@ public class TabOverlayRenderHandler {
 
     @SubscribeEvent
     public void onOverlayRender(RenderGameOverlayEvent.Post event) {
-        if (event.isCanceled() || event.getType() != RenderGameOverlayEvent.ElementType.PLAYER_LIST) {
+        if (event.getType() != RenderGameOverlayEvent.ElementType.PLAYER_LIST) {
             return;
         }
 
+        requestTickTime();
+    }
+
+    @SubscribeEvent
+    public void onOverlayRenderSingleplayer(RenderGameOverlayEvent.Post event) {
+        if (event.getType() != RenderGameOverlayEvent.ElementType.CHAT) {
+            return;
+        }
+
+        Minecraft mc = Minecraft.getMinecraft();
+        if (mc.gameSettings.keyBindPlayerList.isKeyDown() && mc.isIntegratedServerRunning()) {
+            mc.ingameGUI.getTabList().renderPlayerlist(new ScaledResolution(mc).getScaledWidth(), mc.world.getScoreboard(), null);
+            requestTickTime();
+        }
+    }
+
+    private void requestTickTime() {
         if ((System.currentTimeMillis() - timeStamp) >= 500) {
             TotalDebug.INSTANCE.network.sendToServer(new TickTimeRequestMessage());
             timeStamp = System.currentTimeMillis();
         }
     }
+
 }
