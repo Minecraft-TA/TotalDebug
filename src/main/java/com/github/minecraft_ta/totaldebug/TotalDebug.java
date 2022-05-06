1bc2d4e40e9b14ea3b1853478bb56ac8c65f9a5f
tth05 <36999320+tth05@users.noreply.github.com>
2022-05-06 17:26:17 +0200

    v1.8.0

--- src/main/java/com/github/minecraft_ta/totaldebug/TotalDebug.java
+++ src/main/java/com/github/minecraft_ta/totaldebug/TotalDebug.java
@@ -34,7 +34,7 @@ public class TotalDebug {
 
     public static final String MOD_ID = "total_debug";
     public static final String MOD_NAME = "TotalDebug";
-    public static final String VERSION = "1.7.2";
+    public static final String VERSION = "1.8.0";
 
     @Mod.Instance(MOD_ID)
     public static TotalDebug INSTANCE;
